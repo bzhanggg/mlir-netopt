62634b9ad1eef76d0f42258103bf3ba64839e103
bzhanggg <blz19@scarletmail.rutgers.edu>
2025-10-04 20:48:34 -0400

    add additional verifiers on CreateOp and PopOp

--- src/dialect/SpmcOps.cpp
+++ src/dialect/SpmcOps.cpp
@@ -1,4 +1,11 @@
 #include "src/dialect/SpmcOps.h"
+#include "src/dialect/SpmcDialect.h"
+
+#include <mlir/IR/Builders.h>
+#include <mlir/IR/BuiltinAttributes.h>
+#include <mlir/IR/Diagnostics.h>
+#include <mlir/IR/MLIRContext.h>
+#include <optional>
 
 namespace mlir {
 namespace spmc {
--- src/dialect/SpmcOps.h
+++ src/dialect/SpmcOps.h
@@ -3,9 +3,16 @@
 
 #include "SpmcDialect.h"
 #include "SpmcTypes.h"
+
 #include <mlir/Bytecode/BytecodeOpInterface.h>
+#include <mlir/Interfaces/InferTypeOpInterface.h>
+#include <mlir/Interfaces/SideEffectInterfaces.h>
+
+#include <mlir/IR/BuiltinOps.h>
+#include <mlir/IR/BuiltinTypes.h>
+#include <mlir/IR/Dialect.h>
 
 #define GET_OP_CLASSES
 #include "src/dialect/SpmcOps.h.inc"
 
-#endif
+#endif // SPMC_OPS_H_
